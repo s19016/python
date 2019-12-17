d0e576f5d766ffec5e3f24a05d6fe262c55ea735
s19016 <s19016@std.it-college.ac.jp>
2019-12-17 14:34:35 +0900

    commit

---
+++ react_app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function App() {
+  constructor (props){
+    super(props)
+    this.start = {}
+  }
+  render(){
+    return(
+      <div />
+    )
+  }
+}
+ 
+
+export default App;
