08e11dc28c97f6775b53b42e2fd030759ab38419
s19016 <s19016@std.it-college.ac.jp>
2020-02-05 16:49:53 +0900

    a

---
+++ electron-quick-start/src/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+export default class App extends Component {
+  render () {
+    return (<div>
+      <h1>hello world</h1>
+    </div>)
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'))
